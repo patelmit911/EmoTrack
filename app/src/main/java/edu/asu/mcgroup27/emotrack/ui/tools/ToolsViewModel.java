117c370cfea5f0fb7889bffc57f72eebb1012b1a
Dhaval <dhaval0024@gmail.com>
2019-11-22 17:45:26 -0700

    Initial commit

---
+++ app/src/main/java/edu/asu/mcgroup27/emotrack/ui/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package edu.asu.mcgroup27.emotrack.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
